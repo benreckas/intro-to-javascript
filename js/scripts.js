57b2a411a10d80e1c13368d8a01d59b8690a7c27
benreckas <benreckas@gmail.com>
2018-11-08 08:35:38 -0800

    dayThree init commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// const obj1 = {
+//   name: 'Ben'
+// };
+
+// const obj2 = {
+//   name: 'Ben'
+// };
+
+// const arr1 = ['Cat'];
+// const arr2 = ['Cat'];
+
+
+// const age = prompt('How old are you?');
+
+// console.log(age <= 21);
+
+const num = '42.3';
+console.log(num);
+console.log(+num);
+
