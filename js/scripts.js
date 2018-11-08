28e8f733060dd6f7f27b3ae0c0ab30543fe00f5f
benreckas <benreckas@gmail.com>
2018-11-07 20:39:03 -0800

    dayTwoTakehome done

--- js/scripts.js
+++ js/scripts.js
@@ -1,77 +1,10 @@
-// String
-// var name = "Ben";
+const firstName = prompt('What is your first name?');
+const lastName = prompt('What is your last name?');
 
-// String Object
-// var nameTwo = new String("Ben");
+console.log(firstName);
+alert(lastName);
 
-// Number
-// var luckyNumber = 42;
-// Boolean
-// var boolean = true;
+const birthday = new Date(prompt('What is your birthday?'));
+confirm(`Is your birthday correct: ${birthday}`);
 
-
-// var mystery;
-
-
-// Global Scope
-
-// function() {
-  // Scoped to function
-// }
-
-
-// let name;
-
-// console.log(name);
-
-// {
-//   name = "Ben";
-//   name = "Norma";
-//   console.log(name);
-// }
-
-// console.log(name);
-
-
-
-// const name = "Bob";
-
-// console.log(`Lorem ipsum dolor sit amet adipisicing elit. Mollitia consectetur quia obcaecati dolores ${name} <h1>sdkfg</h1> blanditiis velit iusto qui repellendus suscipit officiis. Non veritatis eveniet alias, praesentium repellendus provident sint laboriosam labore?`);
-
-
-// const date = new Date();
-
-// console.log(date);
-
-// const str = 'sdlgaslkjfhlaskdfhlkasdhfljasdhflkjasdhfljasdhfljashdflkjhasdlfkjhasdlkjfhasdlkjfhsaldkjghadslkfjgbadslfkjb';
-
-// console.log(str.length);
-
-// const num = new Number(42);
-// const num2 = 42;
-
-// const array = [1, 3];
-// const array2 = new Array(1, 3);
-// const arrayMixed = ['Ben', 29, true];
-
-// console.log(arrayMixed);
-
-// const obj = {
-//   name: 'Ben',
-//   age: 29
-// };
-
-// console.log(obj);
-
-// alert('Danger Will Robinson!!!');
-// const response = confirm('Are you sure?');
-
-// console.log(response);
-
-// const favNum = prompt('What is your favorite number?');
-
-// const realNum = parseInt(favNum);
-
-// const num = '55';
-
-// console.log(parseInt(num));
+alert(`Your birthday is: ${birthday}`);
