fa693970a7b893cc77ff721003b10e514a1064cb
benreckas <benreckas@gmail.com>
2018-11-07 20:11:25 -0800

    dayTwo done

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+// String
+// var name = "Ben";
+
+// String Object
+// var nameTwo = new String("Ben");
+
+// Number
+// var luckyNumber = 42;
+// Boolean
+// var boolean = true;
+
+
+// var mystery;
+
+
+// Global Scope
+
+// function() {
+  // Scoped to function
+// }
+
+
+// let name;
+
+// console.log(name);
+
+// {
+//   name = "Ben";
+//   name = "Norma";
+//   console.log(name);
+// }
+
+// console.log(name);
+
+
+
+// const name = "Bob";
+
+// console.log(`Lorem ipsum dolor sit amet adipisicing elit. Mollitia consectetur quia obcaecati dolores ${name} <h1>sdkfg</h1> blanditiis velit iusto qui repellendus suscipit officiis. Non veritatis eveniet alias, praesentium repellendus provident sint laboriosam labore?`);
+
+
+// const date = new Date();
+
+// console.log(date);
+
+// const str = 'sdlgaslkjfhlaskdfhlkasdhfljasdhflkjasdhfljasdhfljashdflkjhasdlfkjhasdlkjfhasdlkjfhsaldkjghadslkfjgbadslfkjb';
+
+// console.log(str.length);
+
+// const num = new Number(42);
+// const num2 = 42;
+
+// const array = [1, 3];
+// const array2 = new Array(1, 3);
+// const arrayMixed = ['Ben', 29, true];
+
+// console.log(arrayMixed);
+
+// const obj = {
+//   name: 'Ben',
+//   age: 29
+// };
+
+// console.log(obj);
+
+// alert('Danger Will Robinson!!!');
+// const response = confirm('Are you sure?');
+
+// console.log(response);
+
+// const favNum = prompt('What is your favorite number?');
+
+// const realNum = parseInt(favNum);
+
+// const num = '55';
+
+// console.log(parseInt(num));
