891754d8dee9dce6c6bc438610eaeb0a25197ead
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2018-11-12 20:52:13 -0800

    control flow lesson

--- js/scripts.js
+++ js/scripts.js
@@ -11,15 +11,77 @@ console.log(name);
 //   if(true) {
 //     console.log('sdfkg');
 //   }
+// // }
+
+// var firstName = 'Paka';
+// console.log('Paka', firstName);
+
+// const name = 'Jose';
+// console.log(
+//   `asdfasdfasdfasdfasdfasdfasdfasdfasdf ${name}  asdfasdfasdfasdfasdfasdfasdfadfadsf`
+// );
+
+// const date = new Date();
+// console.log(date);
+
+// strict equals cares about data types and loose equals does not
+// const arr1 = 'this is good';
+// const arr2 = new String(arr1);
+
+// console.log(arr1 === arr2);
+// console.log(arr1 == arr2);
+
+// const lastName = '';
+
+// if (lastNmae) {
+//   console.log(`You did not provide a last name`);
 // }
 
-var firstName = "Paka";
-console.log("Paka", firstName);
+//Try This:
 
-const name = "Jose";
-console.log(
-  `asdfasdfasdfasdfasdfasdfasdfasdfasdf ${name}  asdfasdfasdfasdfasdfasdfasdfadfadsf`
-);
+// 1. prompt user for name
+// if they provide a name, alert their name...
+/// if no alert user that you need a name...
+// const name = prompt('What is your name?');
 
-const date = new Date();
-console.log(date);
+// if (name) {
+//   alert(`Hello ${name} and welcome to Intro to JS`);
+// } else {
+//   prompt('Please enter your name again');
+// }
+
+//as a ternary operator
+// name ? alert(name) : alert('INvalid name you fool!');
+// if they provide a name, alert their name...
+/// if no alert user that you need a name...
+
+// switch (name) {
+//   case X = alert(`Hello ${name} and welcome to Intro to JS`):
+//     break;
+
+//   case Y = prompt('Please enter your name again! FOOOOOL'):
+//     break;
+
+//   case Z = alert(`thank you ${name}`):
+//     break;
+// }
+
+
+const overage1 = prompt("I am 18 years of age or over. Please answer with 'true' or false'");
+if (overage1 = 'true') {
+} else {
+  prompt("You not old enough...fool");
+}
+
+const overage2 = prompt('Are you less than 80 years of age?  Answer with "true" or "false"');
+if (oveage2 = 'true') {
+  const starWars = prompt("Do you like Star Wars? ") {
+    if (starWars = 'yes') {
+      alert("You are awesome!!!!");
+    };
+  if (overage2 = 'false') {
+    prompt("Do you like prunes");
+  }
+}
+  };
+
