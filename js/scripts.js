6ac001f4dbeda690a154c75272ab90386239b735
benreckas <benreckas@gmail.com>
2018-11-26 11:46:39 -0800

    daySeven prep

--- js/scripts.js
+++ js/scripts.js
@@ -1,80 +1,36 @@
-// function thanksgiving(name, list) {
+// const main = document.querySelector('main');
+// const title = document.createElement('h1');
+// title.textContent = 'Title';
+// const para = document.createElement('p');
+// para.textContent = 'lorem skldg sdfkjhsd sdjfh dfkjh';
 
-//   console.log(`${name} is thankful for:${makeList(list)}`);
-// }
+// main.append(title);
+// main.insertBefore(para, title);
 
-// function christmas(name, list) {
-//   const christmasList = makeList(list);
-
-//   console.log(`${name} wants these things for Christmas:${christmasList}`);
-// }
-
-// function makeList(list) {
-//   let formattedList = '';
-
-//   for(let i = 0; i < list.length; i++) {
-//     formattedList += ` ${i + 1}) ${list[i]},`
+// function fizzBuzz(start, end) {
+//   for(let i = start; i <= end; i++) {
+//     if(i % 3 === 0 && i % 5 === 0) {
+//       printItem(i, 'FizzBuzz');
+//     } else if (i % 3 === 0) {
+//       printItem(i, 'Fizz');
+//     } else if (i % 5 === 0) {
+//       printItem(i, 'Buzz');
+//     } else {
+//       printItem(i);
+//     }
 //   }
-
-//   return formattedList;
-//   console.log(formattedList);
 // }
 
-// thanksgiving('Ben', ['Family', 'Friends']);
-// christmas('Ben', ['To sleep in', 'To play with my daughters']);
-
-// const items = document.querySelectorAll('.item');
-// console.log(items);
-
-
-// function query(queryStr) {
-//   console.log(document.querySelector(queryStr));
+// function printItem(i, str) {
+//   const p = document.createElement('p');
+//   !str ? p.textContent = i : p.textContent = `${i} ${str}`;
+//   document.body.appendChild(p);
 // }
 
-// query('body');
+// fizzBuzz(1, 100);
 
-// const header = document.querySelector('#header');
-// console.log(header);
+// const button = document.createElement('button');
+// button.textContent = 'Submit';
+// button.classList = 'btn btn-success';
 
-// header.addEventListener('click', event => header.style.backgroundColor = '#0000ff');
-// header.addEventListener('click', event => header.style.display = 'none');
-
-// document.addEventListener('keydown', e => {
-//   console.log(event);
-
-//   if(e.key === 'Enter') {
-//     alert('You hit the enter key.');
-//   }
-
-//   header.style.color = 'tomato';
-// });
-
-const header = document.querySelector('#header');
-// console.log('header', header);
-const container = document.querySelector('#container');
-// console.log('container', container);
-const btn = document.querySelector('#btn');
-// console.log('btn', btn);
-
-btn.addEventListener('click', () => alert('Button clicked!'));
-container.addEventListener('mouseover', () => container.style.background = 'rebeccapurple');
-document.addEventListener('keyup', () => header.style.color = 'lightgreen');
-// document.addEventListener('click', () => window.location.replace('https://www.google.com'));
-
-const buttons = document.querySelectorAll('button');
-console.log(buttons);
-
-function someFunction(event) {
-  console.log(event);
-}
-
-for(let button of buttons) {
-  button.addEventListener('click', event => someFunction(event));
-}
-
-
-input.value = 56;
-
-// buttons.forEach(button => button.addEventListener('click', () => alert('clicked')));
-
-// buttons.addEventListener('click', () => alert('You clicked me.'));
+// document.body.appendChild(button);
