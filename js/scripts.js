985d8b550a97b7121d3ac2c7740f941a1e897422
benreckas <benreckas@gmail.com>
2018-12-03 17:01:03 -0800

    Prep for dayNine

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,72 @@
-// const main = document.querySelector('main');
-// const title = document.createElement('h1');
-// title.textContent = 'Title';
-// const para = document.createElement('p');
-// para.textContent = 'lorem skldg sdfkjhsd sdjfh dfkjh';
+// function Animal(name, energy) {
+//   this.name = name;
+//   this.energy = energy;
 
-// main.append(title);
-// main.insertBefore(para, title);
-
-// function fizzBuzz(start, end) {
-//   for(let i = start; i <= end; i++) {
-//     if(i % 3 === 0 && i % 5 === 0) {
-//       printItem(i, 'FizzBuzz');
-//     } else if (i % 3 === 0) {
-//       printItem(i, 'Fizz');
-//     } else if (i % 5 === 0) {
-//       printItem(i, 'Buzz');
-//     } else {
-//       printItem(i);
-//     }
+//   this.sayName = function() {
+//     console.log(this.name);
 //   }
 // }
 
-// function printItem(i, str) {
-//   const p = document.createElement('p');
-//   !str ? p.textContent = i : p.textContent = `${i} ${str}`;
-//   document.body.appendChild(p);
+// const dog = new Animal('Dog', 20);
+// const cat = new Animal('Cat', 5);
+
+// dog.sayName();
+// cat.sayName();
+
+// Animal.prototype.run = function(amount) {
+//   this.energy -= 5;
 // }
 
-// fizzBuzz(1, 100);
+// console.log(dog.energy);
+// dog.run();
+// console.log(dog.energy);
 
-// const button = document.createElement('button');
-// button.textContent = 'Submit';
-// button.classList = 'btn btn-success';
+// function Animal(name, energy) {
+//   this.name = name;
+//   this.energy = energy;
 
-// document.body.appendChild(button);
+//   this.sayName = function() {
+//     console.log(this.name);
+//   }
+// }
+
+// function Dog(name, energy, breed) {
+//   Animal.call(this, ...arguments);
+
+//   this.breed = breed;
+// }
+
+// const charlie = new Dog('Charlie', 40, 'Mutt');
+
+// console.log(charlie);
+// charlie.sayName();
+
+// const hawk = new Animal('Hawk', 60, 'Redtail');
+// hawk.sayName();
+
+// class Animal {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   sayName() {
+//     console.log(this.name);
+//   }
+// }
+
+// const ben = new Animal('Ben', 29);
+// console.log(ben);
+// ben.sayName();
+
+// class Dog extends Animal {
+//   constructor(name, age, breed) {
+//     super(...arguments);
+
+//     this.breed = breed;
+//   }
+// }
+
+// const fido = new Dog('Fido', 7, 'Goldendoodle');
+// console.log(fido);
+// fido.sayName();
